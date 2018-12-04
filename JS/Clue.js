5a03c249af3fc398c25f46a12fa2e2bb773f47cf
B3an5 <beantheo@gmail.com>
2018-12-04 15:45:42 -0700

    psudocoded js classes

---
+++ JS/Clue.js
@@ -0,0 +1,10 @@
+class Clue {
+
+  checkAnswer() {
+    //checks if player guess is right 
+    //if so run score multiplyer fn in round.js
+    //if not move to next player method in round.js
+  }
+
+  
+}
---
+++ JS/Game.js
@@ -0,0 +1,25 @@
+class Game {
+
+  init() {
+    //starts game
+    //Round.round++
+  }
+
+  endGame() {
+    //displays winner and presents a restart button
+  }
+
+  startRound() {
+    //Round.round ++
+    //run generate Board function in Board.js
+  }
+
+  endRound() {
+    //clears board 
+  }
+
+  reset() {
+    //Round.round = 0
+    //resets game 
+  }
+}
